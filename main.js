2118b858bafc8c7d0af7d9ce04b40b6fa64f8324
gabicilfone <cilfonegabriel@gmail.com>
2022-12-05 19:50:18 -0300

    fix linters error

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-document.getElementById('form').addEventListener('submit', saveTask);
+function getTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  const tasksView = document.getElementById('tasks');
+  tasksView.innerHTML = '';
+  for (let i = 0; i < tasks.length; i += 1) {
+    const { title } = tasks[i];
+    const { author } = tasks[i];
+
+    tasksView.innerHTML += `
+    <div class="book-card">
+        <p class="book-title">${title}</p>
+        <p class="book-author">${author}</p>
+        <button type="button" class="book-btn" onclick="deleteTask('${title}')">Remove</button>
+    </div>
+    <hr>`;
+  }
+}
+getTasks();
 
 function saveTask(e) {
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
 
-  let task = {
+  const task = {
     title,
-    author
+    author,
   };
 
-  if(localStorage.getItem('tasks') === null) {
-    let tasks = [];
+  if (localStorage.getItem('tasks') === null) {
+    const tasks = [];
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
   } else {
-    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
@@ -25,33 +42,15 @@ function saveTask(e) {
 }
 
 function deleteTask(title) {
-  console.log(title)
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  for(let i = 0; i < tasks.length; i++) {
-    if(tasks[i].title == title) {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (let i = 0; i < tasks.length; i += 1) {
+    if (tasks[i].title === title) {
       tasks.splice(i, 1);
     }
   }
-  
   localStorage.setItem('tasks', JSON.stringify(tasks));
   getTasks();
 }
+deleteTask();
 
-function getTasks() {
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  let tasksView = document.getElementById('tasks');
-  tasksView.innerHTML = '';
-  for(let i = 0; i < tasks.length; i++) {
-    let title = tasks[i].title;
-    let author = tasks[i].author;
-
-    tasksView.innerHTML += `
-    <div class="book-card">
-        <p class="book-title">${title}</p>
-        <p class="book-author">${author}</p>
-        <button type="button" class="book-btn" onclick="deleteTask('${title}')">Remove</button>
-    </div>
-    <hr>`;
-    }
-}
-getTasks();
+document.getElementById('form').addEventListener('submit', saveTask);
